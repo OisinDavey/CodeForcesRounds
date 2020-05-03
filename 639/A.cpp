12cabc2ab662fc8c40ceed99115714581632e91d
OisinDavey <oisindavey02@gmail.com>
2020-05-03 12:33:06 +0100

    Division 2 round 639

---
+++ 639/A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h> 
+
+#define MX_N 5001
+#define INF 100000000
+
+#define mod7 1000000007
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+ll gcd(ull a, ull b){return (a==0)?b:gcd(b%a,a);}
+ll lcm(ull a, ull b){return a*(b/gcd(a,b));}
+
+int kx[8] = {+2,+2,-2,-2,+1,+1,-1,-1};
+int ky[8] = {+1,-1,+1,-1,+2,-2,+2,-2};
+int d9x[9] = {+1,+1,+1,+0,+0,+0,-1,-1,-1};
+int d9y[9] = {+1,+0,-1,+1,+0,-1,+1,+0,-1};
+int dx4[4] = {0, 0, +1, -1};
+int dy4[4] = {+1, -1, 0, 0};
+
+using namespace std;
+
+int main(){
+
+}
