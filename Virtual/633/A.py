9c172b270f9e1f26213428fc1a207fef7d38c124
OisinDavey <oisindavey02@gmail.com>
2020-05-09 23:58:55 +0100

    Racked up Penalties

---
+++ Virtual/633/A.py
@@ -0,0 +1,8 @@
+import math
+import sys
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    print(n)
---
+++ Virtual/633/B.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h> 
+#define MX_N 5001
+#define INF 100000000
+
+#define mod7 1000000007
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+ll gcd(ull a, ull b){return (a==0)?b:gcd(b%a,a);}
+ll lcm(ull a, ull b){return a*(b/gcd(a,b));}
+
+int kx[8] = {+2,+2,-2,-2,+1,+1,-1,-1};
+int ky[8] = {+1,-1,+1,-1,+2,-2,+2,-2};
+int d9x[9] = {+1,+1,+1,+0,+0,+0,-1,-1,-1};
+int d9y[9] = {+1,+0,-1,+1,+0,-1,+1,+0,-1};
+int dx4[4] = {0, 0, +1, -1};
+int dy4[4] = {+1, -1, 0, 0};
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int a[n];
+        for(int i=0;i<n;++i)
+            cin >> a[i];
+        sort(a, a+n);
+        int l = (n-1)/2;
+        int r = l+1;
+        while(1){
+            if(l >= 0)
+                cout << a[l] << ' ';
+            else
+                break;
+            if(r < n)
+                cout << a[r] << ' ';
+            else
+                break;
+            --l;
+            ++r;
+        }cout << endl;
+    }
+}
